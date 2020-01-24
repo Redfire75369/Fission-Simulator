c111137469692e8a49c81e072f1f910f1b4bd278
Redfire <Redfire75369@gmail.com>
2020-01-24 20:35:29 +0800

    Break Infinity Unlocked(Not Working yet

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var gameData = {
 	version: {alpha: 0, beta: 4},
-	fuel: 10,
-	mine: [0, 0, 0, 0],
-	mineCost: [10, 100, 10000, 1000000],
-	mineMult: [1.00, 1.00, 1.00, 1.00],
-	mineCostMult: [100, 1000, 100000, 10000000],
-	mineMultMult: 2.00,
+	fuel: new Decimal(10),
+	mine: [new Decimal(0), new Decimal(0), new Decimal(0), new Decimal(0)],
+	mineCost: [new Decimal("1e+1"), new Decimal("1e+2"), new Decimal("1e+4"), new Decimal("1e+7")],
+	mineMult: [new Decimal(1), new Decimal(1), new Decimal(1), new Decimal(1)],
+	mineCostMult: [new Decimal("1e+2"), new Decimal("1e+3"), new Decimal("1e+5"), new Decimal("1e+8")],
+	mineMultMult: new Decimal(2),
 	currentTab: "production", 
 	currentProdTab: ""
 }
@@ -15,12 +15,12 @@ const elements = ["Thorium", "Uranium", "Plutonium", "Americium"];
 function hardReset() {
 	gameData = {
 	version: {alpha: 0, beta: 4},
-	fuel: 10,
-	mine: [0, 0, 0, 0],
-	mineCost: [10, 100, 10000, 1000000],
-	mineMult: [1.00, 1.00, 1.00, 1.00],
-	mineCostMult: [100, 1000, 100000, 10000000],
-	mineMultMult: 2.00,
+	fuel: new Decimal("1e+1"),
+	mine: [new Decimal(0), new Decimal(0), new Decimal(0), new Decimal(0)],
+	mineCost: [new Decimal("1e+1"), new Decimal("1e+2"), new Decimal("1e+4"), new Decimal("1e+7")],
+	mineMult: [new Decimal(1), new Decimal(1), new Decimal(1), new Decimal(1)],
+	mineCostMult: [new Decimal("1e+2"), new Decimal("1e+3"), new Decimal("1e+5"), new Decimal("1e+8")],
+	mineMultMult: new Decimal(2),
 	currentTab: "production", 
 	currentProdTab: ""
 	}
@@ -40,12 +40,12 @@ function showProdTab(tab) {
 }
 
 function buyMine(a) {
-	if (gameData.fuel >= gameData.mineCost[a]) {
-		gameData.fuel -= gameData.mineCost[a];
+	if (gameData.fuel.gte(gameData.mineCost[a])) {
+		gameData.fuel = gameData.fuel.minus(gameData.mineCost[a]) ;
 		
-		gameData.mine[a] += 1;
-		gameData.mineCost[a] *= gameData.mineCostMult[a];
-		gameData.mineMult[a] *= gameData.mineMultMult;
+		gameData.mine[a] = gameData.mine[a].plus(1);
+		gameData.mineCost[a] = gameData.mineCost[a].multiply(gameData.mineCostMult[a]);
+		gameData.mineMult[a] = gameData.mineMult[a].multiply(gameData.mineMultMult);
 		
 		document.getElementById(e[a] + "Mine").innerText = gameData.mine[a];
 		document.getElementById(e[a] + "MineCost").innerHTML = gameData.mineCost[a];
@@ -53,23 +53,23 @@ function buyMine(a) {
 }
 		
 function update() {
-	var fuelPerSecond = gameData.mine[0] * gameData.mineMult[0];
-	var ThMinePerSecond = gameData.mine[1] * gameData.mineMult[1];
-	var UMinePerSecond = gameData.mine[2] * gameData.mineMult[2];
-	var PuMinePerSecond = gameData.mine[3] * gameData.mineMult[3];
+	let fuelPerSecond = gameData.mine[0].multiply(gameData.mineMult[0]);
+	let ThMinePerSecond = gameData.mine[1].multiply(gameData.mineMult[1]);
+	let UMinePerSecond = gameData.mine[2].multiply(gameData.mineMult[2]);
+	let PuMinePerSecond = gameData.mine[3].multiply(gameData.mineMult[3]);
 	
-	gameData.fuel += fuelPerSecond / 20;
-	gameData.mine[0] += ThMinePerSecond / 20;
-	gameData.mine[1] += UMinePerSecond / 20;
-	gameData.mine[2] += PuMinePerSecond / 20;
+	gameData.fuel += fuelPerSecond.dividedBy(20);
+	gameData.mine[0] += ThMinePerSecond.dividedBy(20);
+	gameData.mine[1] += UMinePerSecond.dividedBy(20);
+	gameData.mine[2] += PuMinePerSecond.dividedBy(20);
 
-	document.getElementById("fuel").innerText = "You have " + Math.round(gameData.fuel * 10) / 10 + " Nuclear Fuel.";
-	document.getElementById("fuelPerSecond").innerText = "You are gaining " + Math. round(fuelPerSecond * 10) / 10 + " Nuclear Fuel per second.";
-	var a;
-	for (a = 0; a < e.length; a++) {
-		document.getElementById(e[a] + "Mine").innerText = Math.round(gameData.mine[a] * 10) / 10;
-		document.getElementById(e[a] + "MineCost").innerText = "Cost: " + Math.round(gameData.mineCost[a] * 10) / 10;
-		document.getElementById(e[a] + "MineMult").innerText = elements[a] + " Mine ×" + Math.round(gameData.mineMult[a] * 10) / 10;
+	document.getElementById("fuel").innerText = "You have " + gameData.fuel + " Nuclear Fuel.";
+	document.getElementById("fuelPerSecond").innerText = "You are gaining " + fuelPerSecond + " Nuclear Fuel per second.";
+
+	for (let a = 0; a < e.length; a++) {
+		document.getElementById(e[a] + "Mine").innerText = gameData.mine[a];
+		document.getElementById(e[a] + "MineCost").innerText = "Cost: " + gameData.mineCost[a];
+		document.getElementById(e[a] + "MineMult").innerText = elements[a] + " Mine ×" + gameData.mineMult[a];
 	}
 }
 
