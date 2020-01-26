15c4ced8cacc535993bc33aa6eed6cb87d1e0a7c
Redfire <Redfire75369@gmail.com>
2020-01-26 10:32:24 +0800

    Notation Button

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ var defaultData = {
 	},
 	multMult: new Decimal(2),
 	meteor: 0,
-	meteorMult: new Decimal(2)
+	meteorMult: new Decimal(2),
+	notation: "scientific",
+	notationNo: 0
 }
 const e = ["Th", "U", "Pu", "Am", "Cm", "Bk", "Cf"];
 const elements = ["Thorium", "Uranium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium"];
@@ -19,6 +21,10 @@ var player = defaultData;
 
 function hardReset() {
 	player = defaultData;
+}
+
+function round(x, dp) {
+	return (Math.round(x * Math.pow(10, dp)) / Math.pow(10, dp));
 }
 
 function buyMine(a) {
@@ -30,9 +36,9 @@ function buyMine(a) {
 		player.mine.cost[a] = player.mine.cost[a].multiply(player.mine.costMult[a]);
 		player.mine.mult[a] = player.mine.mult[a].multiply(player.multMult);
 		
-		document.getElementById(e[a] + "Mine").innerText = scientific(player.mine.amount[a]);
-		document.getElementById(e[a] + "MineCost").innerText = "Cost: " + scientific(player.mine.cost[a]);
-		document.getElementById(e[a] + "MineMult").innerText = elements[a] + " Mine ×" + scientific(player.mine.mult[a]);
+		document.getElementById(e[a] + "Mine").innerText = notation(player.mine.amount[a]);
+		document.getElementById(e[a] + "MineCost").innerText = "Cost: " + notation(player.mine.cost[a]);
+		document.getElementById(e[a] + "MineMult").innerText = elements[a] + " Mine ×" + notation(player.mine.mult[a]);
 	}
 }
 
@@ -70,16 +76,52 @@ function meteor() {
 		}
 	}
 }
-
-
-function scientific(x) {
-	if (x.exponent <=3) {
-		return x;
+function notationChange() {
+	let notations = ["scientific", "logarithm"];
+	let notationsCap = ["Scientific", "Logarithmic"];
+	if (player.notationNo + 1 == notations.length) {
+		player.notation = notations[0];
+		player.notationNo = 0;
+		document.getElementById("notation").innerText = "Notation: " +  notationsCap[0];
 	} else {
-		return Math.round(x.mantissa * 100) / 100 + "e" + x.exponent;
+		player.notation = notations[player.notationNo + 1];
+		player.notationNo += 1;
+		document.getElementById("notation").innerText = "Notation: " +  notationsCap[player.notationNo];
+	}
+}
+function notation(x) {
+	switch(player.notation) {
+		case "scientific":
+			return scientific(x);
+			break;
+		case "logarithm":
+			return logarithm(x);
+			break;
+		default:
+	}
+}
+			
+function scientific(x) {
+	if (x.exponent < 4) {
+		return x
+	} else if (x.mantissa > 9.995) {
+		return 1 + "e" + (x.exponent + 1);
+	} else if (x.mantissa < 9.995) {
+		return round(x.mantissa, 2)+ "e" + x.exponent;
 	}
 }
 
+function logarithm(x) {
+	if (x.exponent < 4) {
+		return x;
+	} else {
+		return "e" + (x.exponent + round(Math.log(x.mantissa, 10), 2));
+	}
+}
+
+function setFuel() {
+	player.fuel = new Decimal("1e+16");
+}
 function update() {
 	let fuelPerSecond = player.mine.amount[0].multiply(player.mine.mult[0]);
 	let ThMinePerSecond = player.mine.amount[1].multiply(player.mine.mult[1]);
@@ -91,20 +133,20 @@ function update() {
 	player.mine.amount[1] = player.mine.amount[1].plus(UMinePerSecond.dividedBy(20));
 	player.mine.amount[2] = player.mine.amount[2].plus(PuMinePerSecond.dividedBy(20));
 
-	document.getElementById("fuel").innerText = "You have " + scientific(player.fuel.floor()) + " Nuclear Fuel.";
-	document.getElementById("fuelPerSecond").innerText = "You are gaining " + scientific(fuelPerSecond.floor()) + " Nuclear Fuel per second.";
+	document.getElementById("fuel").innerText = "You have " + notation(player.fuel.floor()) + " Nuclear Fuel.";
+	document.getElementById("fuelPerSecond").innerText = "You are gaining " + notation(fuelPerSecond.floor()) + " Nuclear Fuel per second.";
 
 	for (let a = 0; a < Math.min(player.meteor + 4, 7); a++) {
-		document.getElementById(e[a] + "Mine").innerText = scientific(player.mine.amount[a].floor());
-		document.getElementById(e[a] + "MineCost").innerText = "Cost: " + scientific(player.mine.cost[a]);
-		document.getElementById(e[a] + "MineMult").innerText = elements[a] + " Mine ×" + scientific(player.mine.mult[a]);
+		document.getElementById(e[a] + "Mine").innerText = notation(player.mine.amount[a].floor());
+		document.getElementById(e[a] + "MineCost").innerText = "Cost: " + notation(player.mine.cost[a]);
+		document.getElementById(e[a] + "MineMult").innerText = elements[a] + " Mine ×" + notation(player.mine.mult[a]);
 	}
 }
 
 /*Load Save*/
 function load_save() {
-	let save = JSON.parse(localStorage.getItem("fissionSimSave"));
-	if(save !== undefined) {
+	if (localStorage.getItem("fissionSimSave") !== null) {
+		let save = JSON.parse(localStorage.getItem("fissionSimSave"));
 		player.version = save.version;
 		player.fuel = new Decimal(save.fuel);
 		for (let a = 0; a < Math.min(player.meteor + 4, 7); a++) {
@@ -114,6 +156,9 @@ function load_save() {
 			player.mine.costMult[a] = new Decimal(save.mine.costMult[a]);
 		}
 		player.multMult = new Decimal(save.multMult);
+		player.meteor = save.meteor;
+		player.meteorMult = new Decimal(save.meteorMult);
+		player.notation = save.notation;
 	}
 	if (player.version === undefined) { player.version = defaultData.version; } 
 	if (player.fuel === undefined) { player.fuel = defaultData.fuel; }
@@ -124,6 +169,9 @@ function load_save() {
 		if (player.mine.costMult[b] === undefined) { player.mine.costMult[b] = defaultData.player.mine.costMult[b]; }
 	}
 	if (player.multMult === undefined) { player.multMult = defaultData.multMult; }
+	if (player.meteor === undefined) { player.meteor = defaultData.meteor; }
+	if (player.meteorMult === undefined) { player.meteorMult = defaultData.meteorMult; }
+	if (player.notation === undefined) { player.notation = defaultData.notation; } 
 }
 
 /*Initialise Game*/
