8a0336cb692618e52e988732ce16652d8772acd9
Redfire <Redfire75369@gmail.com>
2020-09-19 12:43:32 +0000

    Automated Build Process

--- javascripts/react/dist/achievements/Achievement.js
+++ javascripts/react/dist/achievements/Achievement.js
@@ -1,20 +1,20 @@
 const achievementTexts = {
-	11: "Thoranium\nBuy one TBU Pebblebed Reactor.",
-	12: "",
-	13: "",
-	14: "",
-	15: "",
-	16: "",
-	17: "",
-	18: "",
-	21: "",
-	22: "",
-	23: "",
-	24: "",
-	25: "",
-	26: "",
-	27: "",
-	28: ""
+  11: "Thoranium\nBuy one TBU Pebblebed Reactor.",
+  12: "",
+  13: "",
+  14: "",
+  15: "",
+  16: "",
+  17: "",
+  18: "",
+  21: "",
+  22: "",
+  23: "",
+  24: "",
+  25: "",
+  26: "",
+  27: "",
+  28: ""
 };
 
 class AchievementComponent extends ReactStateComponent {
@@ -28,11 +28,10 @@ class AchievementComponent extends ReactStateComponent {
     return /*#__PURE__*/React.createElement("div", {
       className: "flex-col align horizontal-center tooltip " + (this.state.completed ? "achcomplete" : "achlocked")
     }, /*#__PURE__*/React.createElement("img", {
-      className: "achpic",
       src: "resources/images/achievements/" + this.props.id + ".png"
     }), /*#__PURE__*/React.createElement("span", {
       className: "tooltiptext"
-    }, achievementTexts[this.props.id]));
+    }, achievementTexts[this.props.id].split("\n")[0], /*#__PURE__*/React.createElement("br", null), achievementTexts[this.props.id].split("\n")[1]));
   }
 
 }
