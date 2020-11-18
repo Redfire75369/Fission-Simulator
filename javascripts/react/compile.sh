619c83db463829cb4aee97eacf69cf1bf7ef9884
Redfire <Redfire75369@gmail.com>
2020-11-18 22:35:45 +0800

    Fixed hotkeys, options desync, Added redirect to HTTPS

--- javascripts/react/compile.sh
+++ javascripts/react/compile.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-npm exec --no babel dev --watch --out-dir dist
+npm exec --no babel --watch dev --out-dir dist
--- javascripts/react/dist/options/NotationOptionsButton.js
+++ javascripts/react/dist/options/NotationOptionsButton.js
@@ -4,8 +4,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 function NotationOptionsButton() {
-  const [notation, setNotation] = React.useState("Scientific");
+  const [notation, setNotation] = React.useState(notations[player.options.notation]);
 
   function notationChange() {
     player.options.notation = (player.options.notation + 1) % notations.length;
